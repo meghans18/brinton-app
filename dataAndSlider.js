c91f28a7fe4c51a4bc31be8cf90fac6e88efee40
meghans18 <saakmeghan@gmail.com>
2018-05-02 11:34:37 -0500

    Resets slider whenever map tab is closed

--- dataAndSlider.js
+++ dataAndSlider.js
@@ -27019,7 +27019,7 @@ L.Control.SliderControl = L.Control.extend({
 
         // Create a control sliderContainer with a jquery ui slider
         var sliderContainer = L.DomUtil.create('div', 'slider', this._container);
-        $(sliderContainer).append('<div id="leaflet-slider" style="width:800px; margin-top: 50px;"><div class="ui-slider-handle"></div><div id="slider-timestamp" style="width:800px; margin-top:13px; background-color:#FFFFFF; text-align:center; border-radius:5px;"></div></div>');
+        $(sliderContainer).append('<div id="leaflet-slider" style="width:1000px; margin-top: 50px;"><div class="ui-slider-handle"></div><div id="slider-timestamp" style="width:1000px; margin-top:13px; background-color:#FFFFFF; text-align:center; border-radius:5px;"></div></div>');
         //Prevent map panning/zooming while using the slider
         $(sliderContainer).mousedown(function () {
             map.dragging.disable();
