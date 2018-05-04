32609ac81adf8f320b9349edbb379b379a3ac0ab
meghans18 <saakmeghan@gmail.com>
2018-05-04 11:24:09 -0500

    Added comments

--- main.js
+++ main.js
@@ -6,12 +6,16 @@ const ipcMain = require('electron').ipcMain
 let main_win
 
 function createWindow() {
+	//BrowserWindow has a lot of options, look at electron documentation for all possibilities
     main_win = new BrowserWindow({width: 1000, height: 1000, backgroundColor: 'black', frame: false, fullscreen:true})
     main_win.loadURL(url.format({
+		//change the part in quotes for whatever file the window will be tied to
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file',
         slashes: true
     }))
+	//the three windows below are all child windows which means they are tied to the parent (main) window
+	//child windows can take different properties than parent windows
     let nowShowing = new BrowserWindow({parent: main_win, show: false, fullscreen: true, frame: false})
     nowShowing.loadURL(url.format({
         pathname: path.join(__dirname, 'nowShowing.html'),
@@ -34,13 +38,14 @@ function createWindow() {
     //main_win.webContents.openDevTools()
     //nowShowing.webContents.openDevTools()
     //meet.webContents.openDevTools()
-	  //map.webContents.openDevTools()
+	//map.webContents.openDevTools()
 
 
     main_win.on('closed', () => {
         main_win = null
     })
 
+	//used for sending messages back and forth between parent and chid windows
     ipcMain.on('closeShowing', function() {
         nowShowing.hide()
     })
@@ -59,6 +64,7 @@ function createWindow() {
 
 	ipcMain.on('closeMap', function() {
         map.hide()
+		//used to send messages to windows
 		map.webContents.send('resetBtns')
     })
 
